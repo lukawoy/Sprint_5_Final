5e3edcb20411b64b252b71ada51908404ba3e7f7
Luka <goproton@mail.ru>
2024-02-08 00:08:58 +0300

    Довлены константы, убран код вне функции, исправлен докстринг)

--- command_expander.py
+++ command_expander.py
@@ -1,12 +1,21 @@
-# Последняя успешная посылка 106764944
-abbreviated_command = input()
+# Последняя успешная посылка 106842378
 
 
 def command_expander(in_command: str, num_char: int = 1, calls: int = 0):
-    """Функция дешифратор сокращенных сообщений,
-    возвращает расшифрованную строку исключительно с буквами.
     """
+    Возвращает расшифрованную строку.
 
+        Параметры:
+            in_command (str): зашифрованная строка
+            num_char (int): количество символов в подстроке (для рекурсии)
+            calls (int): счетчик глубины рекурсии (для рекурсии)
+
+        Возвращаемое значение:
+            current_str (str): расшифрованная строка
+            index (int): текущий индекс цикла (для рекурсии)
+    """
+    open_square_bracket = '['
+    string_zero = '0'
     length: int = len(in_command)
     current_str: str = ''
     current_num: str = ''
@@ -20,15 +29,15 @@ def command_expander(in_command: str, num_char: int = 1, calls: int = 0):
 
         item: str = in_command[index]
 
-        if item.isdigit():
+        if item.isdecimal():
             current_num += item
-        elif item == '[':
+        elif item == open_square_bracket:
             return_str, skip = command_expander(in_command[index + 1:],
                                                 int(current_num),
                                                 calls=calls + 1)
             index += skip
             current_str += return_str
-            current_num = '0'
+            current_num = string_zero
         elif item.isalpha():
             current_str += item
         else:
@@ -42,4 +51,4 @@ def command_expander(in_command: str, num_char: int = 1, calls: int = 0):
 
 
 if __name__ == '__main__':
-    print(command_expander(abbreviated_command)[0])
+    print(command_expander(input())[0])
